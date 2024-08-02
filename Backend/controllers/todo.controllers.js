1f9cbf114c3cfe332381fcb57ebe1455805e2607
Varun L&T Machine <varun.verma2020@vitstudent.ac.in>
2024-08-02 13:04:49 +0530

    added the Update Todo contoller and route

--- Backend/controllers/todo.controllers.js
+++ Backend/controllers/todo.controllers.js
@@ -18,6 +18,8 @@ const fetch_todo = asyncHandler ( async (req,res) => {
     const { id,sort } = req.query;
 
     if (id) {
+        console.log(`[Controller] Fetch request received with _id:${id}`);
+
         const todo = data.find(todo => todo._id === id)
         if (!todo){
             throw new ApiError(404,"[Controller] Todo not found");
@@ -28,6 +30,8 @@ const fetch_todo = asyncHandler ( async (req,res) => {
     }
 
     if (sort){
+        console.log(`[Controller] Fetch request received with _sort:${sort}`);
+
         if (sort === "byCreatedDate.oldest"){
             data.sort((a,b) => new Date(a.createdAt) - new Date(b.createdAt));
         }
@@ -45,6 +49,7 @@ const fetch_todo = asyncHandler ( async (req,res) => {
         }
     }
     
+    console.log(`[Controller] Successfully fetched todos`);
     const api_response = new ApiResponse(200, "Successfully fetched todos", data);
     res.status(api_response.statusCode).json(api_response);
 });
@@ -54,6 +59,8 @@ const fetch_todo = asyncHandler ( async (req,res) => {
 // adds a new todo
 const add_todo = asyncHandler(async (req,res) => {
     const {title,description,completed,dueDate,priority} = req.body;
+
+    console.log(`[Controller] Add todo request received for _id:${id}`);
 
     if (!(title && description && completed && dueDate && priority)){
         throw new ApiError(404,"[Controller] Required fields are missing in body");
@@ -75,6 +82,7 @@ const add_todo = asyncHandler(async (req,res) => {
     data.push(new_todo);
     await write_db(data);
     
+    console.log(`[Controller] Todo created for _id:${id}`);
     const api_response = new ApiResponse(200,"Successfully created new todo",new_todo);
     res.status(200).json(api_response);
 });
@@ -82,17 +90,39 @@ const add_todo = asyncHandler(async (req,res) => {
 
 
 // Update Todo
+const update_todo = asyncHandler( async (req,res) => {
+    const data = await read_db()
+    const { updated_todo } = req.body;
+    const { id } = req.query;
 
+    console.log(`[Controller] Update request received for _id:${id}`);
+
+    let old_todo = data.find((todo) => todo._id === id);
+    if (!old_todo){
+        throw new ApiError(404,"[Controller] Todo not found");
+    }
+
+    // updating the old todo object
+    updated_todo.updatedAt = new Date().toISOString();
+    Object.assign(old_todo, updated_todo)
+
+    // writing date back
+    await write_db(data);
+
+    console.log("[Controller] Updated Todo successfully.");
+    const api_response = new ApiResponse(201,"Successfully updated Todo",old_todo);
+    res.status(200).json(api_response);
+});
 
 
 
 // Delete Todo
-
+const delete_todo = asyncHandler( async (req,res) => {});
 
 
 
 // Mark as Done
-
+const markDone_todo = asyncHandler( async (req,res) => {});
 
 
 
@@ -100,4 +130,7 @@ export {
     landing,
     add_todo,
     fetch_todo,
+    update_todo,
+    delete_todo,
+    markDone_todo
 }
--- Backend/routes/todos.routes.js
+++ Backend/routes/todos.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { landing, add_todo, fetch_todo } from "../controllers/todo.controllers.js";
+import { landing, add_todo, fetch_todo, update_todo } from "../controllers/todo.controllers.js";
 
 const router = Router();
 
@@ -7,5 +7,6 @@ const router = Router();
 router.route("/").get(landing)                  //Endpoint = localhost:8000/api/v1/todos/
 router.route("/create").post(add_todo)          //Endpoint = localhost:8000/api/v1/todos/create
 router.route("/fetch").get(fetch_todo)          //Endpoint = localhost:8000/api/v1/todos/fetch
+router.route("/update").patch(update_todo)      //Endpoint = localhost:8000/api/v1/todos/update
 
 export default router;
